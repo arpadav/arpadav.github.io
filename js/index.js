1291634f1e7b714e83105ff32fd13f0302146213
aavoros <aavoros@ncsu.edu>
2020-10-04 22:22:36 -0400

    rotation, tabs, and functions

--- js/index.js
+++ js/index.js
@@ -17,26 +17,30 @@ var name_rot_crux = ["left center"];
 var name_scale_in = [1.15];
 var name_scale_out = [1];
 
+console.log(document.getElementsByClassName('tab'));
+
 init();
 
 var name_div = document.getElementById('name');
 name_div.addEventListener("click", function() {
   if (click_enable) {
     click_enable = 0;
-    if(rotated){
+    if (rotated) {
       name_div.style.animation = `rotate_horz ${anim_len}s forwards`;
-      // name_div.style.webkitAnimation  = `rotate_horz ${anim_len}s forwards`;
+      name_div.style.webkitAnimation  = `rotate_horz ${anim_len}s forwards`;
       rotated = 0;
     } else {
       name_div.style.animation = `rotate_side ${anim_len}s forwards`;
-      // name_div.style.webkitAnimation  = `rotate_side ${anim_len}s forwards`;
+      name_div.style.webkitAnimation  = `rotate_side ${anim_len}s forwards`;
       rotated = 1;
     }
+    set_tab_visibility(rotated);
     setTimeout(function(){ click_enable = 1; }, anim_len * 1000);
   }
 });
 
 function init() {
+  // console.log(document.getElementsByClassName('tabs'));
   init_css_animation();
 
   console.log("screen width: " + screen.width);
@@ -51,6 +55,12 @@ function init() {
 
 function init_css_animation() {
   let keyframes = find_keyframes_rule("rotate_side");
+
+  // console.log(create_css_transform_rule(0,
+  //   "translate", true, name_anim_begin_pos,
+  //   "rotate", true, name_anim_begin_rot,
+  //   "transform-origin", false, name_rot_crux,
+  //   "scale", true, name_scale_out));
 
   keyframes.deleteRule("0%");
   keyframes.appendRule(create_css_transform_rule(0,
@@ -84,58 +94,106 @@ function init_css_animation() {
     "scale", true, name_scale_out));
 }
 
-// returns string for CSS rule adding
+function set_tab_visibility(visible) {
+  let tabs = document.getElementsByClassName("tab");
+  let len = tabs.length;
+
+  if (visible) for (i = 0; i < len; i++) tabs[i].style.visibility = "visible";
+  else for (i = 0; i < len; i++) tabs[i].style.visibility = "hidden";
+}
+
+// find any CSS property from text
+// STRING:
+// TYPE.cssText.match(/(?<=PROP:)(\s*?)(.*?)(?=\"\s*?\;)/g)[0].match(/(?<=\")(.*?)$/g)[0];
+// TYPE.cssText.match(RegExp(`(?<=${PROP}:)(\\s*?)(.*?)(?=\\"\\s*?\\;)`, 'g'))[0].match(/(?<=\")(.*?)$/g)[0];
+// RAW:
+// TYPE.cssText.match(/(?<=PROP:)(\s*?)(.*?)(?=\s*?\;)/g)[0].trim();
+// TYPE.cssText.match(RegExp(`(?<=${PROP}:)(\\s*?)(.*?)(?=\\s*?\\;)`, 'g'))[0].trim();
+function get_css_property(type, prop, str) {
+  if (str) {
+    try { return type.cssText.match(RegExp(`(?<=${prop}:)(\\s*?)(.*?)(?=\\"\\s*?\\;)`, 'g'))[0].match(/(?<=\")(.*?)$/g)[0]; }
+    catch(err) { return null; }
+  } else {
+    try { return type.cssText.match(RegExp(`(?<=${prop}:)(\\s*?)(.*?)(?=\\s*?\\;)`, 'g'))[0].trim(); }
+    catch(err) { return null; }
+  }
+}
+
+// used in adding a rule for transformations
+// used in initialization, and (somewhat) beyond
+// returns a string (rule) to be added to the CSS rules
+// input format: (0-100 percent, string func1, bool trans_dep1, array params1, string func2, ...
 function create_css_transform_rule () {
+  // save args
   let this_args = arguments;
 
+  // get percent, begin 'rule' string to return
   let percent = this_args[0];
   let rule = percent + "% { ";
 
-  let first_transform_dependence_idx = 0;
+  // initialize arrays for transform dependent and independent functions
+  trans_dep_idx = [];
+  trans_indep_idx = [];
+
+  // populate said arrays
   for (var i = 2; i < this_args.length; i += 3) {
-    if (arguments[i]) {
-      first_transform_dependence_idx = i;
-      break;
-    }
+    if (this_args[i]) trans_dep_idx.push(i - 1); else trans_indep_idx.push(i - 1);
   }
 
-  if (first_transform_dependence_idx) {
-    transforms.forEach(function(item, idx) {
-      for (var i = first_transform_dependence_idx - 1; i < this_args.length; i += 3) {
-        if (this_args[i + 1]) {
-          let function_name = this_args[i];
-          let args = this_args[i + 2];
-          rule = (i == first_transform_dependence_idx - 1) ? rule + item + ": " : rule + " ";
-          rule = rule + function_name + "(";
-          args.forEach(function(item, idx) {
-            rule = (idx != args.length - 1) ? rule + item + ", " : rule + item + ")";
-          });
-        }
-      }
-      rule = rule + "; ";
-    });
-  }
-  for (var i = 1; i < this_args.length; i += 3){
-    if (!this_args[i + 1]) {
-      let function_name = this_args[i];
-      let args = this_args[i + 2];
-      rule = rule + function_name + ": ";
+  // append 'rule' string with transform functions first
+  transforms.forEach(function(t_item) {
+    let more_trans_dep = 0;
+    trans_dep_idx.forEach(function(idx) {
+      let function_name = this_args[idx];
+      let args = this_args[idx + 2];
+
+      rule = (!more_trans_dep) ? rule + t_item + ": " : rule + " ";
+      more_trans_dep = 1;
+      rule = rule + function_name + "(";
+
       args.forEach(function(item, idx) {
-        rule = (idx != args.length - 1) ? rule + item + ", " : rule + item;
+        rule = (idx != args.length - 1) ? rule + item + ", " : rule + item + ")";
       });
-      rule = rule + "; ";
-    }
-  }
+    });
+    rule = rule + "; ";
+  });
+
+  // then, append independent functions/properties
+  trans_indep_idx.forEach(function(idx) {
+    let function_name = this_args[idx];
+    let args = this_args[idx + 2];
+
+    rule = rule + function_name + ": ";
+    args.forEach(function(item, idx) {
+      rule = (idx != args.length - 1) ? rule + item + ", " : rule + item;
+    });
+    rule = rule + "; ";
+  });
+
   return rule + " }";
 }
 
-// uses CSS DOM to change CSS rules
-// used in initialization
+// uses CSS DOM to look through CSS rules
+// used in initialization of keyframe animations
 function find_keyframes_rule(rule) {
   let ss = document.styleSheets;
   for (var i = 0; i < ss.length; ++i) {
     for (var j = 0; j < ss[i].cssRules.length; ++j) {
       if (ss[i].cssRules[j].type == window.CSSRule.KEYFRAMES_RULE && ss[i].cssRules[j].name == rule) {
+        return ss[i].cssRules[j];
+      }
+    }
+  }
+  return null;
+}
+
+// uses CSS DOM to look through CSS rules
+// used in finding CSS styles
+function find_selectortext_rule(rule) {
+  let ss = document.styleSheets;
+  for (var i = 0; i < ss.length; ++i) {
+    for (var j = 0; j < ss[i].cssRules.length; ++j) {
+      if (ss[i].cssRules[j].selectorText == rule) {
         return ss[i].cssRules[j];
       }
     }

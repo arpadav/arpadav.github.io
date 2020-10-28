6d2e8373fdde8355285dc402dead101161d66b3c
aavoros <aavoros@ncsu.edu>
2020-10-28 11:33:46 -0400

    mobile5

--- public/js/index.js
+++ public/js/index.js
@@ -46,35 +46,45 @@ var tabs_pivot = ["left center"];
 init();
 
 function init() {
-  // resize on load
-  resize_divs();
-
-  // name clicking listener
   let name_div = document.getElementById('name');
   name_div.style.cursor = "pointer";
-  name_div.addEventListener("click", name_click);
+  name_div.addEventListener("click", debug_change_name);
 
-  // tabs head clicking listener
-  let tabs_title = document.getElementsByClassName('head');
-  for (i = 0; i < tabs_title.length; i++) {
-    tabs_title[i].style.cursor = "pointer";
-    tabs_title[i].addEventListener("click", toggle_collapse);
-  }
+  // // resize on load
+  // resize_divs();
+  //
+  // // name clicking listener
+  // let name_div = document.getElementById('name');
+  // name_div.style.cursor = "pointer";
+  // name_div.addEventListener("click", name_click);
+  //
+  // // tabs head clicking listener
+  // let tabs_title = document.getElementsByClassName('head');
+  // for (i = 0; i < tabs_title.length; i++) {
+  //   tabs_title[i].style.cursor = "pointer";
+  //   tabs_title[i].addEventListener("click", toggle_collapse);
+  // }
+  //
+  // // add listener for resizing the window
+  // window.addEventListener("resize", function(){
+  //   clearTimeout(resize_timeout);
+  //   resize_timeout = setTimeout(resize_divs, 100);
+  // });
+  //
+  // // key event triggers name_click
+  // // keys: enter, space, numbers, letters
+  // document.body.onkeyup = function(e) {
+  //   if(e.keyCode == 13 || e.keyCode == 32 || (e.keyCode >= 48 && e.keyCode <= 90)) name_click();
+  // }
+  //
+  // // testing ---------------------------------------------
+  // // testing ---------------------------------------------
+}
 
-  // add listener for resizing the window
-  window.addEventListener("resize", function(){
-    clearTimeout(resize_timeout);
-    resize_timeout = setTimeout(resize_divs, 100);
-  });
-
-  // key event triggers name_click
-  // keys: enter, space, numbers, letters
-  document.body.onkeyup = function(e) {
-    if(e.keyCode == 13 || e.keyCode == 32 || (e.keyCode >= 48 && e.keyCode <= 90)) name_click();
-  }
-
-  // testing ---------------------------------------------
-  // testing ---------------------------------------------
+// debug
+function debug_change_name() {
+  let name_div = document.getElementById('name');
+  name_div.children[0].innerHTML = name_div.children[0].innerHTML + "1";
 }
 
 // =======================================================================
@@ -186,6 +196,7 @@ function call_anchors() {
 // rotate name on click, disable clicking for animation length
 // change visibility of other elements depending on status
 function name_click() {
+  // debug_change_name();
   let name_div = document.getElementById('name');
   let tabs = document.getElementsByClassName('tab');
   if (click_enable) {

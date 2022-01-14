79278a59898ba87d9733600f744e6208c41ad15c
aavoros <aavoros@ncsu.edu>
2022-01-13 22:35:39 -0500

    updating projects, almost done

--- views/partials/sidebar.js
+++ views/partials/sidebar.js
@@ -13,7 +13,7 @@ function get_nolink_pages(params, json_arr) {
 
 // read from header.json
 function create_sidebar_html(params, json_arr) {
-  let sidebar_html = '<li><img style=\"width: 268px;\" src="../../../resources/images/alligator.png"/></li>'
+  let sidebar_html = '<li><a href=\"../../../gator/\" target=\"_parent\"><img style=\"width: 268px;\" src="../../../resources/images/alligator.png"/></a></li>'
 
   let page = window.location.href.split("/");
   page = page[page.length - 1].split(".")[0];
@@ -28,11 +28,12 @@ function create_sidebar_html(params, json_arr) {
   if (links) {
     let parents = json_arr['parents'];
     for (let i = 0; i < parents.length; i++) {
-      sidebar_html = sidebar_html + '<li><h3>' + parents[i]['name'] + '</h3></li>';
+      sidebar_html = sidebar_html + '<li><h3><b>' + parents[i]['name'] + '</b></h3></li>';
       let children = parents[i]['children'];
       for (let j = children.length - 1; j >= 0; j--) { // backwards, for rough chronological
         sidebar_html = sidebar_html + '<li><a ';
         sidebar_html = sidebar_html + 'href=\"../../../projects/' + children[j]['dir'] + '\" target=\"_parent\"><i>' + children[j]['title'] + '</i></a></li>';
+        if (j > 0) sidebar_html = sidebar_html + '<li>§</li>';
       }
     }
   }

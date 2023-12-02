cad2a92473783a0e5341bd27332c1b106a9b4125
aavoros <aavoros@ncsu.edu>
2023-12-01 22:41:52 -0500

    no more dropdown, just links. expanding personal notes

--- views/partials/notes.js
+++ views/partials/notes.js
@@ -1,6 +1,7 @@
-import {readJSON, create_link_listing} from '../../public/js/technicals.js';
+import {readJSON, create_link_listing, create_iframes} from '../../public/js/technicals.js';
+import init from '../../public/js/projects.js';
 
 // read from notes_academic.json
 readJSON('../partials/notes_academic.json', 'academic', create_link_listing);
 // read from notes_personal.json
-// readJSON('../partials/notes_personal.json', 'personal', create_link_listing);
+readJSON('../partials/notes_personal.json', ['personal', init], create_iframes);
